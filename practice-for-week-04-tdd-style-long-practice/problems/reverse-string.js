528b02412858484a556f71f35ad61775e42512cc
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-17 16:14:56 -0800

    update

--- practice-for-week-04-tdd-style-long-practice/problems/reverse-string.js
+++ practice-for-week-04-tdd-style-long-practice/problems/reverse-string.js
@@ -1,3 +1,14 @@
-module.exports = function reverseString(string) {
-  // Your code here
-};
+const reverseString = (string) => string.split('').reverse().join('')
+
+
+// console.log(reverseString('string'))
+// console.log(reverseString(['string', 'banana']))
+// console.log(reverseString(26))
+
+
+module.exports = {reverseString};
+
+// module.exports = function reverseString() {
+
+
+// }
--- practice-for-week-04-tdd-style-long-practice/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/reverse-string-spec.js
@@ -1,1 +1,28 @@
 // Your code here
+const {reverseString}  = require("../problems/reverse-string");
+
+const chai = require("chai");
+const expect = chai.expect;
+
+
+describe("reverseString(string)", () => {
+    it("should be a string", function () {
+        const str1 = "string"
+        const revStr = reverseString(str1); // "gnirts"
+        const strRev = "gnirts"
+        expect(revStr).to.equal(strRev)
+       // expect(revStr).to.be.true;        //dont really need it
+    })
+    it("It should not be a number", () => {
+        const num1 = 3;
+        expect(() => reverseString(num1).to.throw(Error))
+        //const revStr2 = reverseString(num1); // not safe to use
+        //expect(revStr2).to.be.false       // dont really need it
+    })
+    it("it should not be a array", () => {
+        const arr1 = [];
+        //const revStr3 = reverseString([]) // not safe to use
+        expect(() => reverseString(arr1).to.throw(Error))
+        // expect(revStr3).to.be.false          //dont really need it
+    })
+})
